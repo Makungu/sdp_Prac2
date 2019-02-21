965de2a7974b07578495b7c28195112d276898f9
Makungu Chansa <G17C9313@ict.ru.ac.za>
2019-02-21 16:05:21 +0200

    added all .csproj, .cs and .sln files

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,25 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //Console.WriteLine(prefix(Hello));
+        }
+
+        public string Prefix(string input)
+        {
+            int length = 0;
+            string[] words = input.Split(" ");
+            for (int n =0; n < input.Length;n++)
+            {
+                length++;
+            }
+            return $"{length},{words.Length}:{input} ";
         }
     }
+   
+   
 }
